02a9a60691ec35c1c91c14e361540f7ef47894c3
Christopher Pinel <cwpinel@gmail.com>
2014-03-26 19:45:03 -0600

    Initial Commit

---
+++ Out of this World/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  Out of this World
+//
+//  Created by Christopher Pinel on 2014-03-26.
+//  Copyright (c) 2014 Christopher Pinel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/CPViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPViewController.h
+//  Out of this World
+//
+//  Created by Christopher Pinel on 2014-03-26.
+//  Copyright (c) 2014 Christopher Pinel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPViewController : UIViewController
+
+@end
